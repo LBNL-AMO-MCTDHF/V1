4ee0c843b88ee73f5775034fa2b9e3efd1ec61b8
dan haxton <dhaxton@devo.lbl.gov>
2015-06-05 19:57:21 -0700

    maskflag bugfix bugfix

--- MCTDH.SRC/SINCDVR/init_sinc_new.f90
+++ MCTDH.SRC/SINCDVR/init_sinc_new.f90
@@ -224,17 +224,17 @@ subroutine init_project(inspfs,spfsloaded,pot,halfniumpot,rkemod,proderivmod,ski
         maskfunction(jj)%rmat(:)=1d0
         do ii=1,masknumpoints
 
-!!$           rsum=cos(pi * i / (masknumpoints+1) )
+           rsum=cos(pi * ii / (masknumpoints+1) )
+
+!!$           rsum=0.5d0 * ( cos( 2*pi * ii / (masknumpoints+1) ) + 1d0 )
 
            i=masknumpoints+1-ii
-           rsum=0.5d0 * ( cos( 2*pi * i / (masknumpoints+1) ) + 1d0 )
 
            if (i.ge.ilow(jj).and.i.le.ihigh(jj)) then
               maskfunction(jj)%rmat(i+1-ilow(jj)) = maskfunction(jj)%rmat(i+1-ilow(jj)) * rsum
            endif
 
            i=gridpoints(jj)-masknumpoints+ii
-           rsum=0.5d0 * ( cos( 2*pi * i / (masknumpoints+1) ) + 1d0 )
 
            if (i.ge.ilow(jj).and.i.le.ihigh(jj)) then
               maskfunction(jj)%rmat(i+1-ilow(jj)) = maskfunction(jj)%rmat(i+1-ilow(jj)) * rsum
