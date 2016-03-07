4f05edc7adc4c1873ec0580da70a16de5b2b9895
dan haxton <dhaxton@devo.lbl.gov>
2016-03-07 05:38:49 -0800

    fft_ct_paropt removed from sinc_params

--- MCTDH.SRC/SINCDVR/sinc_params.f90
+++ MCTDH.SRC/SINCDVR/sinc_params.f90
@@ -10,14 +10,15 @@ implicit none
 logical :: orbparflag=.false.
 !! integer :: orbparlevel=3  !!  in namelist, but not in myparams
 
-!! THE FOLLOWING FLAG IS THEN RELEVANT.  Option for parallel KE matvec, rate limiting step.
+logical :: transmultflag=.true.      !! less communication for ke parlevel 2,3
+
+!! If .not.transmultflag, option for parallel KE matvec. (toepflag for ke not implemented 3d)
 integer :: zke_paropt=1   !! 0=sendrecv 1=SUMMA (bcast before) 2=reduce after
 
 !! fft_batchdim: determines batch size for matrix elements and 
 !! fft_circbatchdim: determines sub batch size for FFT 
-!!    defaults set small (less memory, more MPI messages) to avoid MPI problems when doing large 
-!!    calculations.  Otherwise bigger values will be faster.  There is a message size sweet spot
-!!    on many machines.
+!!    defaults set =1 not =2 to avoid MPI problems when doing large calculations.  Otherwise 
+!!    2 might be faster.  There is a message size sweet spot on many machines.
 integer :: fft_batchdim=1     !! 1 = do nspf matrix elements in nspf batches (less memory)
                               !! 2 = do nspf^2 in one batch (faster unless MPI problems)
 integer :: fft_circbatchdim=1 !! 0,1,2, circbatchdim < batchdim; larger faster unless MPI problems
@@ -25,17 +26,18 @@ integer :: fft_mpi_inplaceflag=1     !! fft_mpi_inplaceflag:
                                      !!  0 = out-of-place fft, out-of-place fft inverse
                                      !!      3d FFT + (summa/circ C.T. depending on fft_ctflag)
                                      !!  1 = 3 x (1d FFT , all-to-all index transposition)
-integer :: num_skip_orbs=0
-integer :: orb_skip(200)=-1
+integer :: num_skip_orbs=0         !! skip eigenfunctions for initial orbitals
+integer :: orb_skip(200)=-1        !!   which ones to skip
 
 integer :: toothnsmall=40
 integer :: toothnbig=240
 
 integer :: numcenters=1
-integer :: centershift(3,100)=0    !! grid point index for each center
+integer :: centershift(3,100)=0    !! positions of nuclei, in units of spacing/2.  if numpoints is
+                                   !! odd then centershift must be even and vice versa.
 real*8 :: nuccharges(100)=2d0
 
-integer :: numpoints(100)=15
+integer :: numpoints(100)=15       !! points on a side of the cube, total basis size numpoints^3
 real*8 :: spacing=0.25d0
 
 integer :: orblanorder=500         !! krylov order for block lanczos orbital calculation
@@ -49,24 +51,21 @@ real*8 :: capstart(100)=0.001d0    !! Capmode=0 is
 real*8 :: capstrength(100)=0.01d0  !!   v_i(r)= capstrength_i*max(0,r-capstart_i)^cappower_i
 real*8 :: mincap=0d0 , maxcap=1d30 !! V_CAP = -i* max(mincap,min(maxcap,sum_i v_i))
 
-integer :: maskflag=0
-integer :: masknumpoints=0
-
 integer :: scalingflag=0           !! 1 = SMOOTH EXTERIOR COMPLEX SCALING
 real*8 :: scalingdistance=10000d0  !! atomic units (bohr)
 real*8 :: smoothness=5             !! atomic units (bohr)
 real*8 :: scalingtheta=0d0         !! scaling angle
-real*8 :: scalingstretch=1d0         !! stretching factor
+real*8 :: scalingstretch=1d0       !! stretching factor
 
- real*8 :: tinv_tol=1d-3
-
-!! construct virtual orbtials using density based on loaded orbitals
-integer :: ivoflag=0
-real*8 :: loadedocc(200)=2d0
+integer :: ivoflag=0               !! construct virtual orbitals using density based on 
+real*8 :: loadedocc(200)=2d0       !!    loaded orbitals
 !! XXSNIPXX 
 !! INTERNAL
 
-logical :: transmultflag=.true.
+integer :: maskflag=0
+integer :: masknumpoints=0
+
+real*8 :: tinv_tol=1d-3
 
 integer :: orbtargetflag=0
 DATATYPE :: orbtarget=DATAZERO
