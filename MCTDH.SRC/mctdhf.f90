472c697d55b51dfec94c9082185f58bf1aa6d0d8
dan haxton <nad_xahnot@fastmail.com>
2017-01-12 23:41:47 -0800

    update of manual v1.33 in readme manual and main

--- MCTDH.SRC/mctdhf.f90
+++ MCTDH.SRC/mctdhf.f90
@@ -239,7 +239,7 @@ program mctdhf
   write(mpifileptr, *) "   AMO Theory Group, Lawrence Berkeley Laboratory"
   write(mpifileptr, *) "     D J Haxton, C W McCurdy, T N Rescigno, K V Lawler, J Jones, "
   write(mpifileptr, *) "     B Abeln, X Li . . ."
-  write(mpifileptr, *) "                             VERSION 1.32 "
+  write(mpifileptr, *) "                             VERSION 1.33 "
   write(mpifileptr, *) "       Copyright 2016 the regents of the University of California"
   write(mpifileptr, *)
 #ifdef REALGO
